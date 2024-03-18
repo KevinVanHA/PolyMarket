222e8d7c975b1c92799d322959b7ca18b18418ff
tonyshenNL <tony@northernlabs.ca>
2024-03-18 13:12:14 -0400

    adding marketplace + collection contract addresses

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -7,11 +7,11 @@ export const NETWORK = Mumbai;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
-export const MARKETPLACE_ADDRESS = "0x83c57ec0dF015eef8401fFb7CB7f66CfA8b6Ff55";
+export const MARKETPLACE_ADDRESS = "0x6d5BA6220645bB6d7d1c4735ccA6E840515DA563";
 
 // 3. The address of your NFT collection smart contract.
 export const NFT_COLLECTION_ADDRESS =
-  "0x67b40fC017863743C520cDAec1B367BE1FD721f0";
+  "0x9Dea3e7235Fb9eC0331A4cCf3C2A5270fFfeE9C3";
 
 // (Optional) Set up the URL of where users can view transactions on
 // For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
